8940cf41c0922718bc81da852992f78a3fcc8d19
amasotti <toniomasotti@gmail.com>
2026-02-17 17:51:15 +0100

    chore: sample files

---
+++ sample-files/custom-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyCustomWidget from './MyCustomWidget';
+
+function CustomComponent() {
+  return (
+    <div>
+      <MyCustomWidget title="Hello" />
+    </div>
+  );
+}
+
+export default CustomComponent;
